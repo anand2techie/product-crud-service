cefff3cc50b07efd49fbf940e58a0c8040827196
Ananthanarayanan Ramachandran <ananth.ramachandran@heavenhr.de>
2020-02-28 11:11:04 +0100

    Added security config

---
+++ src/main/java/com/challenge/productservice/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.challenge.productservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity //Simple WebSecurityConfiguration for the purpose of the exercise.
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/products/**", "/swagger**")
+            .permitAll().anyRequest()
+            .authenticated()
+            .and()
+            .httpBasic()
+            .and().csrf().disable();
+    }
+}
